4f518371b218a27a08022c94dda0095c403d26e8
Gon Gis <g0ng0n.dev@gmail.com>
2021-12-07 20:20:12 +0100

    finishing the Layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from "./components";
+import { Navbar, Exchanges, HomePage, Cryptocurrencies, CryptoDetails, News } from "./components";
 import './App.css';
 
 const App = () => {
@@ -12,10 +12,30 @@ const App = () => {
                 <Navbar />
             </div>
             <div className="main">
+                <Layout >
+                    <div className="routes">
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/exchanges" element={<Exchanges />} />
+                            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                            <Route path="/news" element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
 
-            </div>
-            <div className="footer">
-
+                <div className="footer">
+                    <Typography.Title  level={5} style={{color: 'white', textAlign: 'center'}}>
+                        Cryptoverse <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     );
